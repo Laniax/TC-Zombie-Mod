3feb0746b228163b7bc63b9edd7fad9a4621dd82
root <amort11@hotmail.com>
2011-12-17 22:16:16 +0100

    Push misc changes made so we can get started working together on Kaelima's server

--- src/server/scripts/Custom/Zombie_event.cpp
+++ src/server/scripts/Custom/Zombie_event.cpp
@@ -1,3 +1,25 @@
+/*
+ * Copyright (C) 2008-2011 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2006-2009 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* Explanation
+<TODO>
+*/
+
 enum ZombieInvasion
 {
     SPELL_ATTACK_1H         = 42880,
@@ -7,6 +29,7 @@ enum ZombieInvasion
     SPELL_COUNTER           = 18282,
     SPELL_EXPLODE           =  7670, // Used by Bomb Bot
     SPELL_EXPLODE_2         = 71495, // Used by Proximity Mine - triggers knockback (40191)
+    SPELL_FLAME_BREATH      = 49161,
 
     GAMEOBJECT_BARRICADE    = 293610,
     GAMEOBJECT_ANVIL        = 293611,
